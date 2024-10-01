783f3d6493fd7f2c697fae93802fc530b556c763
douglasrmachado <douglasroqmachado@gmail.com>
2024-10-01 20:01:52 -0300

    initializing js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let btnNext = document.querySelector('.next')
+let btnBack = document.querySelector('.back')
+
+let container = document.querySelector('.container')
+let list = document.querySelector('.container .list ')
+let thumb = document.querySelector('.container .thumb')
+
